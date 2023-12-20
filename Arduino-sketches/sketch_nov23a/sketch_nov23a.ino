8894ca568d5fc92145e0fef5bb0aae33980350ca
Abdelrahman Tharwat <abdelrahman_thr2002@hotmail.com>
2023-12-20 14:10:17 +0200

    missed

--- Arduino-sketches/sketch_nov23a/sketch_nov23a.ino
+++ Arduino-sketches/sketch_nov23a/sketch_nov23a.ino
@@ -1,36 +1,53 @@
+#include "DHT.h"
 #include <SoftwareSerial.h>
-#include <DHT.h>
 
-#define DHTPIN 2  // Replace with the actual pin connected to the DHT sensor
-#define DHTTYPE DHT11  // Replace with the sensor type (DHT11, DHT21, DHT22)
+#define DHTPIN 2
 
-SoftwareSerial ESPSerial(16, 17); // RX pin 16, TX pin 17
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11 // DHT 11
+//#define DHTTYPE DHT22 // DHT 22 (AM2302), AM2321
+//#define DHTTYPE DHT21 // DHT 21 (AM2301)
 
+SoftwareSerial espSerial(1, 3);
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  Serial.begin(9600); // Start Serial for debugging
-  ESPSerial.begin(9600);
-
+  Serial.begin(115200);
+  espSerial.begin(115200);
   dht.begin();
+  delay(200);
 }
 
 void loop() {
-  if (ESPSerial.available()) {
-    String command = ESPSerial.readStringUntil('\n');
-
-    if (command == "read_sensor") {
-      float temperature = dht.readTemperature();
-      float humidity = dht.readHumidity();
-
-      if (isnan(temperature) || isnan(humidity)) {
-        ESPSerial.println("Failed to read from sensor");
-      } else {
-        ESPSerial.print("Temperature:");
-        ESPSerial.print(temperature);
-        ESPSerial.print(",Humidity:");
-        ESPSerial.println(humidity);
-      }
+  if (espSerial.available() > 0) {
+    String server = espSerial.readStringUntil('\n'); // Read until newline character
+    
+    // Switch-case block to handle different components
+    if (server == "LCD") {
+      // Code to handle LCD component
+    } else if (server == "Motion Sensor") {
+      // Code to handle Motion Sensor component
+    } else if (server == "TempHum") {
+      // Code to handle Temperature and Humidity component
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      
+      Serial.print("H: ");
+      Serial.print(h);
+      Serial.print("% ");
+      Serial.print("T: ");
+      Serial.print(t);
+      Serial.print("C");
+      
+      String str = "coming from arduino: H= " + String(h) + " T= " + String(t);
+      espSerial.println(str);
+    } else if (server == "Moorse Code") {
+      // Code to handle Morse Code component
+    } else {
+      // Handle cases where the server string doesn't match any known components
+      // You can add default behavior or error handling here
     }
   }
+
+  delay(200); // Adjust delay as needed for stability
 }
--- scripts/usb-output.py
+++ scripts/usb-output.py
@@ -1,11 +1,31 @@
-import serial
+import socket
 
-ser = serial.Serial('/dev/ttyACM0', 9600)  # Replace with your port and baud rate
+# Define the server's IP address and port
+server_ip = "0.0.0.0"  # Listen on all available interfaces
+server_port = 5008  # Use the same port as configured in the ESP32-CAM code
 
-# Assuming Arduino sends data in format: "temperature,humidity\n"
+# Create a TCP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP address and port
+server_socket.bind((server_ip, server_port))
+
+# Listen for incoming connections (max backlog of connections set to 5)
+server_socket.listen(5)
+
+print(f"Listening for incoming connections on port {server_port}...")
+
 while True:
-    if ser.in_waiting > 0:
-        data = ser.readline().decode('utf-8').strip()
-        print(f'Received data: {data}')
+    # Accept incoming connection
+    client_socket, client_address = server_socket.accept()
 
+    print(f"Connection from {client_address} has been established.")
 
+    # Receive data from the client
+    received_data = client_socket.recv(1024).decode("utf-8")
+    print(f"Received data: {received_data}")
+
+    # Optionally, you can add your own logic here to process the received data
+
+    # Close the connection with the client
+    client_socket.close()
