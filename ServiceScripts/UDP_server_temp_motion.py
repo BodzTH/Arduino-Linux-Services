d5a70d22ddb9c04c6b584c6555e3a6ca561d078a
OmarEssam2003 <119080233+OmarEssam2003@users.noreply.github.com>
2023-12-30 16:09:12 +0200

    Update UDP_server_temp_motion.py

--- ServiceScripts/UDP_server_temp_motion.py
+++ ServiceScripts/UDP_server_temp_motion.py
@@ -8,22 +8,22 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the address and port
 server_address = ('127.0.1.1', 65432)  # Change this to your desired server address and port
 server_socket.bind(server_address)
+size=0
 x=1
+array={}
 try:
-    while True:
+    while size<5:
         print("Waiting for a message...")
         # Receive data from the client and client's address
         data, address = server_socket.recvfrom(1024)  # Adjust the buffer size as needed
         print(f"Received {len(data)} bytes from {address}")
         print("Data:", data.decode())
+        array.append(data.decode())
 
         # Sending a response back to the client
         sys.argv[x]
         #user_input=input("Enter your message: ")
         server_socket.sendto(sys.argv[x].encode(), address)        
-        if(sys.argv[x]=="q"):
-            print("Server stopped by the user.")
-            break
 
         x=x+1
 
@@ -33,3 +33,5 @@ except KeyboardInterrupt:
 finally:
     # Close the server socket
     server_socket.close()
+    for x in array:
+      print(x)
